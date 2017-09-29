7cefd5dde0b4c690625797d0130d1e6b3065e764
Kristofer Jarl <kristofer.jarl@pagero.com>
2017-09-29 23:06:48 +0200

    Calculating flow at pressure difference

--- src/main/scala/com/sootsafe/engine/StepCalculation.scala
+++ src/main/scala/com/sootsafe/engine/StepCalculation.scala
@@ -11,20 +11,34 @@ object StepCalculation {
     }
   }
 
-  def calculateFlowFromNodeToNextJunction(startNode: Option[LinkedNode], pressureLossTable: Seq[PressureLossEntry]): Double = startNode match {
+  def calculateResistanceFromNodeToNextJunction(startNode: Option[LinkedNode], pressureLossTable: Seq[PressureLossEntry]): Double = startNode match {
     case Some(x) =>
       // A "trick" if the startNode is a junction itself
-      calculateFlowFromNodeToNextJunction(x.parent, pressureLossTable, findPressureEntry(x.nodeModule.key, pressureLossTable))
+      calculateResistanceFromNodeToNextJunction(x.parent, pressureLossTable, findPressureEntry(x.nodeModule.key, pressureLossTable))
     case None => 0d
   }
 
-  private def calculateFlowFromNodeToNextJunction(startNode: Option[LinkedNode], pressureLossTable: Seq[PressureLossEntry], aggregator: Double = 0d): Double = {
+  private def calculateResistanceFromNodeToNextJunction(startNode: Option[LinkedNode], pressureLossTable: Seq[PressureLossEntry], aggregator: Double = 0d): Double = {
     startNode match {
-      case Some(n) =>
-        if (n.nodeModule.isJunction) aggregator
-        else calculateFlowFromNodeToNextJunction(n.parent, pressureLossTable, aggregator + findPressureEntry(n.nodeModule.key, pressureLossTable))
-      case None =>
-        aggregator
+      case Some(n) if n.nodeModule.isJunction => aggregator
+      case Some(n) => calculateResistanceFromNodeToNextJunction(n.parent, pressureLossTable, aggregator + findPressureEntry(n.nodeModule.key, pressureLossTable))
+      case None => aggregator
     }
+  }
+
+  def calculateFlowFromNodeToNextJunction(startNode: Option[LinkedNode]): Double = startNode match {
+    case Some(x) => calculateFlowFromNodeToNextJunction(x.parent, x.nodeModule.ssInfo.capacity.getOrElse(0d))
+    case None => 0d
+  }
+
+  private def calculateFlowFromNodeToNextJunction(startNode: Option[LinkedNode], max: Double = 0d): Double = startNode match {
+    case Some(x) if x.nodeModule.isJunction => Math.max(max, x.nodeModule.ssInfo.capacity.getOrElse(0d))
+    case Some(x) => calculateFlowFromNodeToNextJunction(x.parent, Math.max(max, x.nodeModule.ssInfo.capacity.getOrElse(0d)))
+    case None => max
+  }
+
+  def calculateFlowAtPressureDifference(startNode: Option[LinkedNode], firePressure: Double, regularPressure: Double): Double = {
+    val originalFlow = calculateFlowFromNodeToNextJunction(startNode)
+    originalFlow * Math.sqrt(firePressure/regularPressure)
   }
 }
--- src/test/scala/com/sootsafe/engine/StepCalculationTest.scala
+++ src/test/scala/com/sootsafe/engine/StepCalculationTest.scala
@@ -12,6 +12,7 @@ import org.scalatest.{Matchers, WordSpecLike}
 class StepCalculationTest extends WordSpecLike with Matchers {
 
   implicit val formats: Formats = DefaultFormats + NodeSerializer
+  private val linkedModel = createLinkedModel("/defaultTestData.json")
 
   private def createLinkedModel(fileName: String): LinkedNode = {
     val stream: InputStream = getClass.getResourceAsStream(fileName)
@@ -22,8 +23,7 @@ class StepCalculationTest extends WordSpecLike with Matchers {
 
   "Step Calculation" must {
 
-    "properly calculate the pressure from target node to the next junction" in {
-      val linkedModel = createLinkedModel("/defaultTestData.json")
+    "properly calculate the resistance from target node to the next junction" in {
       val valueResolver: ValueResolver = new ValueResolver {}
 
       val outletNode = linkedModel.locateOutletNode()
@@ -33,13 +33,12 @@ class StepCalculationTest extends WordSpecLike with Matchers {
 
       val secondJunction = firstJunction.findNextJunction().thisNode
 
-      val pressure = StepCalculation.calculateFlowFromNodeToNextJunction(secondJunction, pressureLossTable)
+      val pressure = StepCalculation.calculateResistanceFromNodeToNextJunction(secondJunction, pressureLossTable)
 
       pressure should be(3.164158585861644)
     }
 
-    "properly calculate the pressure from first junction to the next" in {
-      val linkedModel = createLinkedModel("/defaultTestData.json")
+    "properly calculate the resistance from first junction to the next" in {
       val valueResolver: ValueResolver = new ValueResolver {}
 
       val outletNode = linkedModel.locateOutletNode()
@@ -48,13 +47,12 @@ class StepCalculationTest extends WordSpecLike with Matchers {
 
       val pressureLossTable = new PressureLoss(valueResolver).calculatePressureLoss(firstJunction, outletNode.get)
 
-      val pressure = StepCalculation.calculateFlowFromNodeToNextJunction(firstJunction.parent, pressureLossTable)
+      val pressure = StepCalculation.calculateResistanceFromNodeToNextJunction(firstJunction.parent, pressureLossTable)
 
       pressure should be(2.400202446689589)
     }
 
-    "properly traverse through a chain" in {
-      val linkedModel = createLinkedModel("/defaultTestData.json")
+    "properly traverse to calculate resistance through a chain" in {
       val valueResolver: ValueResolver = new ValueResolver {}
 
       val outletNode = linkedModel.locateOutletNode()
@@ -66,11 +64,25 @@ class StepCalculationTest extends WordSpecLike with Matchers {
       var pressure: Double = 0
       var junction: Option[LinkedNode] = firstJunction.get.parent
       while(junction.nonEmpty) {
-        pressure += StepCalculation.calculateFlowFromNodeToNextJunction(junction, pressureLossTable)
+        pressure += StepCalculation.calculateResistanceFromNodeToNextJunction(junction, pressureLossTable)
         junction = junction.get.findNextJunction().thisNode
       }
 
       pressure should be(54.36596993157699)
+    }
+
+    "calculate flow from fire node to first junction" in {
+      val fireNode = linkedModel.locateTargetNode()
+      val flow = StepCalculation.calculateFlowFromNodeToNextJunction(fireNode)
+
+      flow should be(17.0)
+    }
+
+    "calculate flow at pressure difference" in {
+      val fireNode = linkedModel.locateTargetNode()
+      val fireFlow = StepCalculation.calculateFlowAtPressureDifference(fireNode, 1000, 22)
+
+      fireFlow should be (114.61397661875115)
     }
   }
 }
