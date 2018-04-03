6c6209f82643c64ee85c5e4df4b20eb4b804379c
Kristofer Jarl <kristofer.jarl@pagero.com>
2018-04-03 17:44:34 +0200

    Moved from decimal digits to significant digits. Not sure which one is better yet.

--- src/main/scala/com/sootsafe/reporting/ReportFormat.scala
+++ src/main/scala/com/sootsafe/reporting/ReportFormat.scala
@@ -1,5 +1,7 @@
 package com.sootsafe.reporting
 
+import java.math.MathContext
+
 trait ReportFormat {
   def write(num: BigDecimal): String
   def write(num: Option[Double]): String
@@ -8,9 +10,11 @@ trait ReportFormat {
 object DefaultReportFormat extends ReportFormat {
 
   private val decimalPlaces = 2
+  private val significantFigures = 4
 
   def write(num: BigDecimal): String = {
-    num.setScale(decimalPlaces, BigDecimal.RoundingMode.HALF_UP).toDouble.toString
+//    num.setScale(decimalPlaces, BigDecimal.RoundingMode.HALF_UP).toDouble.toString
+    num.round(new MathContext(significantFigures)).doubleValue().toString
   }
 
   override def write(num: Option[Double]): String = if (num.isEmpty) "" else write(num.get)
