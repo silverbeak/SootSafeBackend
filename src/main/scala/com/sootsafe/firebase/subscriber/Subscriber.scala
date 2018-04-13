174c1e7b860e65654a26e6243780d5dc2f78c9f2
Kristofer Jarl <kristofer.jarl@pagero.com>
2018-04-13 08:39:43 +0200

    Use reference/linking to requests instead of subscribing directly to the root of the request

--- src/main/scala/com/sootsafe/firebase/subscriber/Subscriber.scala
+++ src/main/scala/com/sootsafe/firebase/subscriber/Subscriber.scala
@@ -40,9 +40,49 @@ object Subscriber {
     }
   }
 
+  private def createEventListener(messageChannel: Channel[(String, DocumentReference)],
+                                  db: Firestore): EventListener[DocumentSnapshot] = new EventListener[DocumentSnapshot] {
+
+    override def onEvent(snapshot: DocumentSnapshot, error: FirestoreException): Unit = {
+      if (error != null) {
+        System.err.println("Listen failed: " + error)
+        throw new Exception(s"Listen to firebase failed", error)
+      } else {
+        // A new reference to a request has been detected
+        // Fetch the data in that spot, and send it to singleDocumentUpdateToJson
+        Try(snapshot.getData.get("id")) match {
+          case Success(id: String) =>
+            val request = db.collection("releaseRate").document(id).get().get()
+            singleDocumentUpdateToJson(messageChannel, request) match {
+              case Success(json) =>
+                messageChannel.write(json, request.getReference)
+
+                // Delete the original reference to the request data, so that we don't pick it up again if we restart the backend
+                snapshot.getReference.delete()
+              case Failure(e) => ???
+            }
+          case _ => ???
+        }
+      }
+    }
+  }
+
   def subscribe(db: Firestore, messageChannel: Channel[(String, DocumentReference)]): Unit = {
-    val docRef = db.collection("releaseRate")
-    docRef.addSnapshotListener(createSnapshotListener(messageChannel))
+    val docRef = db.collection("releaseRateRequests").document("requests")
+    //    docRef.addSnapshotListener(createSnapshotListener(messageChannel))
+    docRef.addSnapshotListener(createEventListener(messageChannel, db))
+  }
+
+  private def singleDocumentUpdateToJson(messageChannel: Channel[(String, DocumentReference)], snapshot: DocumentSnapshot): Try[String] = {
+    implicit val formats: DefaultFormats.type = DefaultFormats
+
+    if (snapshot.exists()) {
+      System.out.println("Added data: " + snapshot.getData + ", Reference: " + snapshot.getReference.getPath)
+      val gson = new Gson()
+      Success(gson.toJson(snapshot.getData))
+    } else {
+      Failure(new Exception(s"Document with id ${snapshot.getId} does not exist"))
+    }
   }
 
   private def singleDocumentUpdateToJson(messageChannel: Channel[(String, DocumentReference)], change: DocumentChange): Try[String] = {
