db314e54ce8f0d04b9558a60dcd5bcaf5bf630c1
Kristofer Jarl <kristofer.jarl@pagero.com>
2018-04-27 13:58:31 +0200

    Nicer fonts in report

--- src/main/scala/com/sootsafe/reporting/Fixture.scala
+++ src/main/scala/com/sootsafe/reporting/Fixture.scala
@@ -28,6 +28,12 @@ object Fixture {
         |\\usepackage{pgfplots}
         |\\usepackage{threeparttable}
         |\\usepackage{multirow}
+        |% Change base font for entire document to latin modern
+        |\usepackage{lmodern}
+        |\renewcommand{\familydefault}{\sfdefault}   % Supprime le serif (dyslexie)
+        |\usepackage[font=sf, labelfont={sf}]{caption}
+        |% End font update
+        |
         |\\newwatermark*[allpages,color=red!50,angle=45,scale=3,xpos=0,ypos=0]{SOOTSAFE.COM}
         |
         |% \\DeclarePairedDelimiter\\abs{\\lvert}{\\rvert}%
