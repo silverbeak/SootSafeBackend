9ea553bac10a81f6e4f8404148572ecf7b7990da
Kristofer Jarl <kristofer.jarl@pagero.com>
2018-05-05 13:20:30 +0200

    Multiple requests don't clash anymore

--- src/main/scala/com/sootsafe/firebase/subscriber/Subscriber.scala
+++ src/main/scala/com/sootsafe/firebase/subscriber/Subscriber.scala
@@ -15,35 +15,44 @@ import scala.util.{Failure, Success, Try}
 
 object Subscriber {
 
-  private def createEventListener(messageChannel: Channel[(String, DocumentReference)],
-                                  db: Firestore): EventListener[DocumentSnapshot] = new EventListener[DocumentSnapshot] {
+  import scala.collection.JavaConversions._
 
-    override def onEvent(snapshot: DocumentSnapshot, error: FirestoreException): Unit = {
+  private def createEventListener(messageChannel: Channel[(String, DocumentReference)],
+                                  db: Firestore): EventListener[QuerySnapshot] with Object {
+    def onEvent(snapshot: QuerySnapshot, error: FirestoreException): Unit
+  } = new EventListener[QuerySnapshot] {
+
+    override def onEvent(snapshot: QuerySnapshot, error: FirestoreException): Unit = {
       if (error != null) {
         System.err.println("Listen failed: " + error)
         throw new Exception(s"Listen to firebase failed", error)
       } else {
         // A new reference to a request has been detected
         // Fetch the data in that spot, and send it to singleDocumentUpdateToJson
-        Try(snapshot.getData.get("id")) match {
-          case Success(id: String) =>
-            val request = db.collection("releaseRate").document(id).get().get()
-            singleDocumentUpdateToJson(messageChannel, request) match {
-              case Success(json) =>
-                messageChannel.write(json, request.getReference)
+        val changes = snapshot.getDocumentChanges.iterator().toSeq
+        changes.map {
+          change: DocumentChange =>
+            Try(change.getDocument.getData.get("id")) match {
+              case Success(id: String) if change.getType == DocumentChange.Type.ADDED =>
+                val request = db.collection("releaseRate").document(id).get().get()
+                singleDocumentUpdateToJson(messageChannel, request) match {
+                  case Success(json) =>
+                    messageChannel.write(json, request.getReference)
 
-                // Delete the original reference to the request data, so that we don't pick it up again if we restart the backend
-                snapshot.getReference.delete()
-              case Failure(e) => ???
+                    // Delete the original reference to the request data, so that we don't pick it up again if we restart the backend
+                    change.getDocument.getReference.delete()
+                  case Failure(e) => ???
+                }
+              case x =>
+                throw new Exception(s"Unexpected data when looking for atex requests: $x")
             }
-          case _ => ???
         }
       }
     }
   }
 
   def subscribe(db: Firestore, messageChannel: Channel[(String, DocumentReference)]): Unit = {
-    val docRef = db.collection("releaseRateRequests").document("requests")
+    val docRef = db.collection("releaseRateRequests")
     //    docRef.addSnapshotListener(createSnapshotListener(messageChannel))
     docRef.addSnapshotListener(createEventListener(messageChannel, db))
   }
