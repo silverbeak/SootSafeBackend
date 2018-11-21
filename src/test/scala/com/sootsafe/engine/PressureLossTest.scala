f7b3642bf240aa0fc7140aec75401297b1743b37
Kristofer Jarl <kristofer.jarl@trollmoj.com>
2018-11-21 22:37:56 +0100

    Added test for default value of pressure loss value

--- src/test/scala/com/sootsafe/engine/PressureLossTest.scala
+++ src/test/scala/com/sootsafe/engine/PressureLossTest.scala
@@ -2,9 +2,9 @@ package com.sootsafe.engine
 
 import java.io.InputStream
 
-import com.sootsafe.model.{Model, ModelBuilder}
+import com.sootsafe.model._
 import com.sootsafe.serializers.NodeSerializer
-import com.sootsafe.valuetable.{FakeValueResolver, ValueResolver}
+import com.sootsafe.valuetable.FakeValueResolver
 import org.json4s.native.Serialization.read
 import org.json4s.{DefaultFormats, Formats}
 import org.scalatest.{Matchers, WordSpecLike}
@@ -16,21 +16,13 @@ class PressureLossTest extends WordSpecLike with Matchers {
   "PressureLossTest" must {
 
     "calculatePressureLoss" in {
-      val stream: InputStream = getClass.getResourceAsStream("/defaultTestData.json")
-      val defaultJson = scala.io.Source.fromInputStream(stream).mkString
+      val valueResolver = FakeValueResolver
+      val model = readModel("/defaultTestData.json")
 
-      val valueResolver: ValueResolver = FakeValueResolver
-
-      val model = read[Model](defaultJson)
       new ModelBuilder(model).buildModel() match {
         case Right(message) => fail(s"Expected model. Got error message: $message")
         case Left(linkedModel) =>
-          val fireNode = linkedModel.locateTargetNode().get
-          val outletNode = linkedModel.locateOutletNode()
-          val firstJunction = fireNode.findNextJunction().thisNode.get
-
-          val pressureLossTable = new PressureLoss(valueResolver).calculatePressureLoss(firstJunction, outletNode.get)
-          val pressureLoss = pressureLossTable.foldLeft(0d)((agg, pl) => pl.pressureLoss + agg)
+          val (pressureLossTable: Seq[PressureLossEntry], pressureLoss: Double) = aggregatePressureLoss(valueResolver, linkedModel)
           pressureLoss should be(55.544371375939654)
 //          pressureLoss should be(54.64488178026986)
           pressureLossTable.length should be(13)
@@ -38,5 +30,46 @@ class PressureLossTest extends WordSpecLike with Matchers {
 
     }
 
+    "pick supplied pressure loss before calculated one" in {
+      val model = readModel("/defaultTestData.json")
+      val valueResolver = FakeValueResolver
+
+      // Here, we take out a single pipe from the model extracted from json,
+      // replace it with one that has a pressureloss value, and make sure that that value is being used
+      val specificNode = model.nodeDataArray.find(_.key == 8).get
+      val replacementPipe = Pipe.apply(specificNode.key, specificNode.ssInfo.copy(pressureloss = Some(33.3)))
+      val newNodeDataArray = model.nodeDataArray.filterNot(_.key == 8) :+ replacementPipe
+      val newModel = model.copy(nodeDataArray = newNodeDataArray)
+
+      new ModelBuilder(newModel).buildModel() match {
+        case Right(message) => fail(s"Expected model. Got error message: $message")
+        case Left(linkedModel) =>
+          val (pressureLossTable: Seq[PressureLossEntry], pressureLoss: Double) = aggregatePressureLoss(valueResolver, linkedModel)
+
+          // This is where the new value should have changed
+          pressureLoss should be(87.22437137593965)
+          // Still the same length of the array, though
+          pressureLossTable.length should be(13)
+      }
+    }
+
+  }
+
+  private def aggregatePressureLoss(valueResolver: FakeValueResolver.type, linkedModel: LinkedNode) = {
+    val fireNode = linkedModel.locateTargetNode().get
+    val outletNode = linkedModel.locateOutletNode()
+    val firstJunction = fireNode.findNextJunction().thisNode.get
+
+    val pressureLossTable = new PressureLoss(valueResolver).calculatePressureLoss(firstJunction, outletNode.get)
+    val pressureLoss = pressureLossTable.foldLeft(0d)((agg, pl) => pl.pressureLoss + agg)
+    (pressureLossTable, pressureLoss)
+  }
+
+  private def readModel(fileName: String): Model = {
+    val stream: InputStream = getClass.getResourceAsStream(fileName)
+    val defaultJson = scala.io.Source.fromInputStream(stream).mkString
+
+
+    read[Model](defaultJson)
   }
 }
